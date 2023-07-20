d5aa291e4f20db118501211616327efe7d42b881
ulisesafcdev <ulisesafc2020@gmail.com>
2023-07-19 22:54:49 -0600

    Scripts de ejemplo String

---
+++ js/strings.js
@@ -0,0 +1,18 @@
+let texto = "Esta es una cadena de texto"
+let textoII = 'Esta es otra cadena de texto'
+let textoIII = `Otra cadena de texto`
+
+/* console.log(texto) */
+/* console.log(textoII)
+console.log(textoIII) */
+
+let textoIV = new String("Cadena de texto")
+console.log(textoIV)
+
+/* console.log(texto instanceof String)
+console.log(typeof texto) */
+
+/* console.log('------------------') */
+
+console.log(textoIV instanceof String)
+console.log(typeof textoIV)
