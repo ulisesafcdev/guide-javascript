8954b4e0d2eb9bcb8e3a04bbb15004e41ae1d0f2
ulisesafcdev <ulisesafc2020@gmail.com>
2023-07-23 21:05:30 -0600

    script de ejemplos guide number

---
+++ js/numbers.js
@@ -0,0 +1,12 @@
+let edad = 24
+let edadII = new Number(23)
+let edadIII = Number(50)
+/* console.log(edadII) */
+
+/* console.log(edad instanceof Number)
+console.log(edadII instanceof Number)
+console.log(edadIII instanceof Number) */
+
+console.log(typeof edad)
+console.log(typeof edadII)
+console.log(typeof edadIII)
