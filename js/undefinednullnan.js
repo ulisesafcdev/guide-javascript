31b5e2b59987368a3f5c76892f3588a6e5dea5a6
ulisesafcdev <ulisesafc2020@gmail.com>
2023-07-24 20:47:23 -0600

    script ejemplos undefined null NaN

---
+++ js/undefinednullnan.js
@@ -0,0 +1,10 @@
+/* let nombreCompleto
+console.log(nombreCompleto) */
+
+/* let estadoCivil = null
+console.log(estadoCivil) */
+
+let nombre = "odiseo"
+let resultado = nombre * 100
+
+console.log(resultado)
