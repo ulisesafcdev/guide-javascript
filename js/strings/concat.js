7a151a590e3ff08a1caac3c59d9f46c0d13e93bc
ulisesafcdev <ulisesafc2020@gmail.com>
2023-08-04 23:27:35 -0600

    script ejemplo method concat string

---
+++ js/strings/concat.js
@@ -0,0 +1,8 @@
+let saludo = "Buen@s dias/tardes/noche"
+let usuario = "ulisesafcdev"
+
+console.log(saludo.concat(" ", usuario))
+
+let datos = ["Ulises Alexis", "Flores Chavez"]
+
+console.log("".concat(...datos))
