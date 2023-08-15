5d614d2d41f535de86692332cb28323000cf6466
ulisesafcdev <ulisesafc2020@gmail.com>
2023-08-14 22:43:16 -0600

    script ejemplo condicional switch

---
+++ js/switch.js
@@ -0,0 +1,20 @@
+let verduras = "manzanas"
+
+switch (verduras) {
+    case "peras":
+        console.log(`La libra de peras cuesta $2.15`)
+        break;
+    case "uvas":
+        console.log(`La libra de uvas cuesta $3.55`)
+        break;
+    case "fresas":
+        console.log(`La libra de fresas cuesta $1.15`)
+        break;
+    case "manzanas":
+        console.log(`La libra de manzanas cuesta $1.25`)
+        break;
+
+    default:
+        console.log(`Busca ayuda a un asociado`)
+        break;
+}
