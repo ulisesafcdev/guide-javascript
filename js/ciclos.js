872c61f2b3f5db09b105dbff527c693320969f82
ulisesafcdev <ulisesafc2020@gmail.com>
2023-08-27 21:54:40 -0600

    script ejemplo bucle for

---
+++ js/ciclos.js
@@ -0,0 +1,21 @@
+/* for (let i = 1; i <= 10; i++) {
+    console.log(i)
+} */
+
+/* let i = 1
+let m = 8
+
+do {
+    let res = m * i
+    console.log(`${m} * ${i} = ${res}`);
+    i++
+} while (i <= 10); */
+
+let contador = 1
+let multiplicador = 7
+
+while (contador <= 10) {
+    let resultado = multiplicador * contador
+    console.log(`${multiplicador} * ${contador} = ${resultado}`)
+    contador++
+}
