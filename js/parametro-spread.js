3fae53c9ff01589431e2e81e305197b653ff05d8
ulisesafcdev <ulisesafc2020@gmail.com>
2023-09-04 23:02:51 -0600

    script ejemplo parametro spread

---
+++ js/parametro-spread.js
@@ -0,0 +1,9 @@
+let norteamerica = ["Canada", "EE.UU", "Mexico"]
+let centroamerica = ["Belice", "Guatemala", "El Salvador", "Honduras", "Nicaragua", "Costa Rica", "Panama"]
+
+console.log(norteamerica)
+console.log(centroamerica)
+
+let nortecentro = [...norteamerica, ...centroamerica]
+
+console.log(nortecentro)
