d2fde35a28fa4b158a62d66e23500567a94737cd
ulisesafcdev <ulisesafc2020@gmail.com>
2023-09-06 19:10:07 -0600

    script ejemplo funciones flecha

---
+++ js/funciones-flecha.js
@@ -0,0 +1,10 @@
+/* const bienvenida = () => {
+    console.log(`Bienvenido...`)
+}
+
+bienvenida() */
+
+const suma = (a, b) => { return a + b }
+
+let resultado = suma(12, 8)
+console.log(resultado)
