92f1be27f7c66938dd10ec3c4462fa7a5296a445
ulisesafcdev <ulisesafc2020@gmail.com>
2023-09-10 15:14:06 -0600

    se continuo con mas ejemplos de prototipos

--- js/prototipos-poo.js
+++ js/prototipos-poo.js
@@ -1,4 +1,4 @@
-function Persona(nombre, apellido, edad) {
+/* function Persona(nombre, apellido, edad) {
     this.nombre = nombre
     this.apellido = apellido
     this.edad = edad
@@ -17,3 +17,76 @@ persona2.show()
 // accediendo a propiedades
 console.log(`Acceso a propiedad de ${persona1.nombre}`)
 console.log(`Acceso a propiedad de ${persona2.nombre}`)
+
+console.log(persona1) */
+
+/* const terrestre = function () {
+    if (ruedas) {
+        console.log(`Tiene ruedas`)  
+    } else {
+        console.log(`NO tiene ruedas`)
+    }
+    console.log(`Medio de transporte terrestre`)
+}
+
+const aereo = function () {
+    if (ruedas) {
+        console.log(`Tiene ruedas`)  
+    } else {
+        console.log(`NO tiene ruedas`)
+    }
+    console.log(`Medio de transporte aéreo`)
+}
+
+const acuatico = function () {
+    if (ruedas) {
+        console.log(`Tiene ruedas`)  
+    } else {
+        console.log(`NO tiene ruedas`)
+    }
+    console.log(`Medio de transporte acuático`)
+} */
+
+function Transporte(ruedas, tipo) {
+    this.ruedas = ruedas
+    this.tipo = tipo
+}
+
+Transporte.prototype.terrestre = function () {
+    if (this.ruedas) {
+        console.log(`Tiene ruedas`)  
+    } else {
+        console.log(`NO tiene ruedas`)
+    }
+    console.log(`Medio de transporte terrestre`)
+}
+
+Transporte.prototype.aereo = function () {
+    if (this.ruedas) {
+        console.log(`Tiene ruedas`)  
+    } else {
+        console.log(`NO tiene ruedas`)
+    }
+    console.log(`Medio de transporte aéreo`)
+}
+
+Transporte.prototype.acuatico = function () {
+    if (this.ruedas) {
+        console.log(`Tiene ruedas`)  
+    } else {
+        console.log(`NO tiene ruedas`)
+    }
+    console.log(`Medio de transporte acuático`)
+}
+
+const auto = new Transporte(true, "vehiculo sedan")
+const helicoptero = new Transporte(false, "aeronave de helices")
+const barco = new Transporte(false, "nave de mar")
+
+/* console.log(auto)
+console.log(helicoptero)
+console.log(barco) */
+
+auto.terrestre()
+helicoptero.aereo()
+barco.acuatico()
