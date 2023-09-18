cb16984b9f95b13288e25c01e41433c72671fe0a
ulisesafcdev <ulisesafc2020@gmail.com>
2023-09-18 17:36:46 -0600

    script expor modules

---
+++ js/modules/mismodulos.js
@@ -0,0 +1,11 @@
+/* export const PI = 3.1416
+export let usuario = "ulisesafcdev"
+export let datos = {
+    pais: "El salvador",
+    capital: "San Salvador"
+}
+export let lenguajes = ["html", "css", "javascript", "php", "mysql"]
+
+export default function saludo () {
+    console.log(`Exportando módulos...`)
+} */
