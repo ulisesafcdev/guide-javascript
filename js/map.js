4df17a47a98f5790917e745b8dba2d1a4e74fe55
ulisesafcdev <ulisesafc2020@gmail.com>
2023-11-02 22:25:35 -0600

    script ejemplo map js

---
+++ js/map.js
@@ -0,0 +1,26 @@
+let mapa = new Map([
+    ["nombres", "leonel andres"],
+    ["apellidos", "messi cuccitini"],
+    ["edad", 35],
+    ["nacionalidad", "argentino"]
+])
+
+// console.log(mapa)
+
+// console.log(mapa.size)
+
+// mapa.set("telefono", "74589632")
+// mapa.set("empleado", true)
+
+// console.log(mapa.has("edad"))
+// console.log(mapa.has("nacionalidad"))
+// console.log(mapa.has("hobbies"))
+
+// console.log(mapa.delete("telefono"))
+// console.log(mapa.delete("empleado"))
+// console.log(mapa.delete("hobbies"))
+
+mapa.clear()
+
+
+console.log(mapa)
